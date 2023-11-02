4ceb023c1263581c01d9eaa653cee3abad6e9790
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-11-02 20:34:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function volume_sphere() {
+	
     //Write your code here
+
+
+	    // Get the input field for the radius
+    var radiusInput = document.getElementById('radius');
+
+    // Get the input field for the volume
+    var volumeInput = document.getElementById('volume');
+
+    // Parse the radius input as a float
+    var radius = parseFloat(radiusInput.value);
+
+    // Check if the radius is a valid number
+    if (!isNaN(radius) && radius >= 0) {
+        // Calculate the volume using the formula
+        var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+        // Set the calculated volume in the volume input field
+        volumeInput.value = volume.toFixed(2); // Rounded to 2 decimal places
+    } else {
+        // If the input is not a valid number, display an error message
+        volumeInput.value = "Invalid input. Please enter a valid positive number.";
+    }
+
+    // Prevent the form from submitting and refreshing the page
+    return false;
+}
   
 } 
 
